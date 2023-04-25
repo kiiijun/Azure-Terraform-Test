624c4043850583443bd141317ad6264c46bea05b
Kijun Nam <snrm123@naver.com>
2023-04-25 16:01:02 +0900

    방화벽에 방화벽 정책 적용

--- hub/firewall.tf
+++ hub/firewall.tf
@@ -4,6 +4,13 @@ resource "azurerm_public_ip" "fw_pip" {
   resource_group_name = azurerm_resource_group.Hub-resource-group.name
   allocation_method   = "Static"
   sku                 = "Standard"
+  zones = [ ]
+  
+}
+resource "azurerm_firewall_policy" "fw-pol" {
+  name = "${var.hub_prefix}-fw-pol"
+  resource_group_name = azurerm_resource_group.Hub-resource-group.name
+  location = azurerm_resource_group.Hub-resource-group.location
 }
 
 resource "azurerm_firewall" "fw" {
@@ -12,16 +19,12 @@ resource "azurerm_firewall" "fw" {
   resource_group_name = azurerm_resource_group.Hub-resource-group.name
   sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
+  zones = [ ]
+  firewall_policy_id = azurerm_firewall_policy.fw-pol.id
 
   ip_configuration {
     name                 = "ip_config"
     subnet_id            = data.azurerm_subnet.azfw_subnet.id
     public_ip_address_id = azurerm_public_ip.fw_pip.id
   }
-}
-
-resource "azurerm_firewall_policy" "fw-pol" {
-  name = "${azurerm_firewall.fw.name}-pol"
-  resource_group_name = azurerm_resource_group.Hub-resource-group.name
-  location = azurerm_resource_group.Hub-resource-group.location
 }
